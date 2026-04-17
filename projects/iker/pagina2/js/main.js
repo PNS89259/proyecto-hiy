13a40de098d03332e96bb777193aa4ddabed81d9
PNS89259 <PNS89259@educastur.es>
2026-04-17 12:09:01 +0200

    feat: Casa de Acogida, contexto histórico y botón JS para subir arriba de la web

--- projects/iker/pagina2/js/main.js
+++ projects/iker/pagina2/js/main.js
@@ -11,3 +11,30 @@ document.addEventListener('DOMContentLoaded', () => {
         revealObserver.observe(el);
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btnTop = document.querySelector('#btnScrollTop');
+    const sectionBienvenidos = document.querySelector('#bienvenidos');
+
+    window.addEventListener('scroll', () => {
+        // Obtenemos la posición de la sección bienvenidos
+        const sectionTop = sectionBienvenidos.offsetTop;
+
+        if (window.scrollY > sectionTop) {
+            btnTop.classList.remove('d-none');
+            btnTop.classList.add('fade-in-btn');
+        } else {
+            btnTop.classList.add('d-none');
+            btnTop.classList.remove('fade-in-btn');
+        }
+    });
+
+    // Acción de volver arriba
+    btnTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+});
